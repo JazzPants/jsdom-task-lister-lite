d49d24f836eda335fc08d3cdae9eeafa77e8d832
JazzPants <leighton.l14@gmail.com>
2021-12-07 21:57:27 +1100

    Added main and delete features

--- src/index.js
+++ src/index.js
@@ -6,9 +6,27 @@ document.addEventListener("DOMContentLoaded", () => {
       let newTaskDesc = document.getElementById("new-task-description").value;
 
       let newList = document.createElement("li");
-
       newList.innerHTML = newTaskDesc;
 
+
+      let deleteButton = document.createElement("button")
+      deleteButton.className = "deleteClass"
+      deleteButton.innerHTML = " x";
+      deleteButton.addEventListener("click", deleteList)
+
+      newList.appendChild(deleteButton);
       document.getElementById("tasks").append(newList)
+
+      function deleteList() {
+        document.getElementById("tasks").removeChild(newList)
+    return console.log("hello world")
+  }
       })
+
+      
+
+
+      
+ 
   });
+
